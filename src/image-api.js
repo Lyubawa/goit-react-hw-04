22d9ecb31597ca7a93549d8887a99a76fe6308bd
Lyubawa <lyubawa81@ukr.net>
2024-03-04 17:38:51 +0200

    hw-react-4-2

--- src/image-api.js
+++ src/image-api.js
@@ -3,11 +3,11 @@ import axios from "axios"
 axios.defaults.baseURL = "https://api.unsplash.com";
 
 export const fetchImages = async (searchQuery) => {
-    const response = await axios.get("/photos", {
+    const response = await axios.get("/search/photos", {
         params: {
             query: searchQuery,
             client_id: "cswiz_hNylYQ32QPH8pmdrnRSyvGkKdHK3VeITwHtIA",
-            perPage: 10,
+            per_page: 12,
         }
     });
     return response.data;
